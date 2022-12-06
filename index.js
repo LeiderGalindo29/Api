aec0a24e2f6bda70e51c95d3b802429d7c8ce2b5
LeiderGalindo29 <104436225+LeiderGalindo29@users.noreply.github.com>
2022-12-05 22:38:36 -0500

    end

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const {connectDB} = require('./src/mongoCone');
+const PORT= process.env.PORT || 3000;
 //Conexión a la BD
 connectDB();
 const app = express()
 //Midlewares
 app.use(express.json());
+app.set('view engine','ejs')
+app.use(express.static('public'));
 
 //Llamado de rutas
 //app.use('/public', express.static('public/upload'));
-//app.use('/public', express.static(__dirname + '/public'));
+app.use('/', express.static('../public/index'));
 app.use('/api', require('./src/routes/canciones.routes'));
 app.use('/api', require('./src/routes/albumes.routes'));
 app.use('/api', require('./src/routes/generos.routes'));
 //app.use("/api",rutasAlbum);
 //app.use("/api",rutasGenero);
 
-app.get('/', (req,res)=>{
-    res.send('Hola')
-})
-app.listen(3000)
+/*app.get('/', (req,res)=>{
+    res.send('./index')
+})*/
+app.listen(PORT)
 
 module.exports=app;
--- src/mongoCone.js
+++ src/mongoCone.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async()=>{
       try{
-            const DB=await mongoose.connect('mongodb+srv://Leider:12345@cluster0.n9alpov.mongodb.net/Disquera?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
+            const DB=await mongoose.connect('mongodb+srv://Leider:12345@cluster0.n9alpov.mongodb.net/Disquera?retryWrites=true&w=majority');
             console.error("Conexión satisfactoria",DB.connection.name); 
       }catch(error){
             console.error(error);
